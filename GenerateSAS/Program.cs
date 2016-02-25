4a4ad5063b98be0a515c29c8942cc655b004ba64
Michael Curd <micurd@microsoft.com>
2016-02-24 17:05:02 -0800

    created file sas token generator

--- GenerateSAS/Program.cs
+++ GenerateSAS/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +26,38 @@ namespace GenerateSAS
 
         private static void GenerateFileSAS()
         {
+            CloudStorageAccount storageAccount = GetStorageAccount();
+
+            //Create the file client object
+            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+
+            //Get a reference to file share
+            Console.WriteLine("Share:");
+            string shareName = Console.ReadLine();
+            CloudFileShare share  = fileClient.GetShareReference(shareName);
+
+            //Get reference to file
+            Console.WriteLine("File:");
+            string fileName = Console.ReadLine();
+            CloudFileDirectory rootDir = share.GetRootDirectoryReference();
+            CloudFile file = rootDir.GetFileReference("abc.txt");
+       
+            //Create SAS
+            SharedAccessFilePolicy sasConstraints = new SharedAccessFilePolicy();
+            sasConstraints.SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-10);
+            sasConstraints.SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(10);
+            sasConstraints.Permissions = SharedAccessFilePermissions.Read;
+
+            //Generate the shared access signature on the blob, setting the constraints directly on the signature.
+            string sasFileToken = file.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            Console.WriteLine(file.Uri + sasFileToken);
+
+        }
+
+        private static CloudStorageAccount GetStorageAccount()
+        {
             Console.WriteLine("Default Endpoints Protocol: 1 = http, 2 = https");
             int choice = int.Parse(Console.ReadLine());
             string defaultEndpointsProtocol = "https";
@@ -40,16 +74,11 @@ namespace GenerateSAS
             Console.WriteLine("Account Key:");
             string accountKey = Console.ReadLine();
             string storageConnectionString = "DefaultEndpointsProtocol=" + defaultEndpointsProtocol + ";AccountName=" + accountName + ";AccountKey=" + accountKey;
+            Console.WriteLine(storageConnectionString);
 
             //Parse the connection string and return a reference to the storage account.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
-
-            //Create the blob client object.
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-
-            //Get a reference to a container to use for the sample code, and create it if it does not exist.
-            CloudBlobContainer container = blobClient.GetContainerReference("sascontainer");
-            container.CreateIfNotExists();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            return storageAccount;
         }
     }
 }
